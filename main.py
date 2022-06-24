0ef95544e58c1fb928c687b2b6dceaa4e87068f2
katpc <kat@MacBook-Pro-katpc.local>
2022-06-24 16:26:59 +0300

    Cookies work requests

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ import requests
 # except JSONDecodeError:
 #     print("Response is not a JSON format")
 
-response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
-first_response = response.history[0]
-second_response = response
-print(first_response.url)
-print(second_response.url)
+# response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+# first_response = response.history[0]
+# second_response = response
+# print(first_response.url)
+# print(second_response.url)
+
+payload = {"login": "secret_login", "password": "secret_pass2"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+
+cookies_value = response1.cookies.get('auth_cookie')
+cookies = {}
+if cookies_value is not None:
+    cookies.update( {'auth_cookie': cookies_value})
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
+
+print(response2.text)

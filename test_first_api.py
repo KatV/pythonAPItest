5fdc15300e8d78cce94754b44be8f840eb593245
katpc <kat@MacBook-Pro-katpc.local>
2022-06-27 18:43:10 +0300

    Parametrized test

--- test_first_api.py
+++ test_first_api.py
@@ -1,9 +1,18 @@
+import pytest
 import requests
 
 class TestFirstApi:
-    def test_hello_call(self):
+    # tuples
+    names = [
+        ("Kate"),
+        ("Anna"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Kate'
+        #name = 'Kate'
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -12,7 +21,11 @@ class TestFirstApi:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
         actual_response_text = response_dict['answer']
         assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
 
